a6c95aa6c90c5688fe8945db41e5be8f215319ee
Eduardo Torres Davila <etdavila10@gmail.com>
2020-07-08 16:30:37 -0700

    New Way of Sorting the Matrix Table

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let mat;
 let gradedDegreesLeft;
 let gradedDegreesTop;
+let shiftPressed = false;
 
 // create variables for DOM elments we will be targeting
 let $tableMain = $("#matrix-table");
-let $tableHead = $("#matrix-head");
 let $tableBody = $("#matrix-body");
 let $optionButtons = $("#options button");
-let $toggleButton = $("#toggle");
+let $innerTable = $(".inner-table");
 
 let oldPos;
 let newPos;
@@ -19,17 +19,15 @@ $(document).ready(function() {
   readInput();
   createTable();
   $(document).keydown(function(event) {
-    if (event.keyCode == 16) {
-      $tableHead.sortable('disable');
-      $tableBody.sortable('enable');
+    if (event.keyCode == 16 && !shiftPressed) {
+      shiftPressed = true;
       switchToRows();
+      createTable();
     }
-  });
-  $(document).keyup(function(event) {
-    if (event.keyCode == 16) {
-      $tableHead.sortable('enable');
-      $tableBody.sortable('disable');
+    else if (event.keyCode == 16 && shiftPressed) {
+      shiftPressed = false;
       switchToCols();
+      createTable();
     }
   });
 });
@@ -57,11 +55,11 @@ function printMatrix() {
   mat.forEach(function (row) {
     strRows.push(row.join(' '));
   });
-  
+
   outputMat = strRows.join("\n");
   outputGradedDegreesLeft = gradedDegreesLeft.join(" ");
   outputGradedDegreesTop = gradedDegreesTop.join(" ");
-  
+
   $('#read-matrix').val(outputMat);
   $("#gradedDegsLeft").val(outputGradedDegreesLeft);
   $("#gradedDegsTop").val(outputGradedDegreesTop);
@@ -69,92 +67,121 @@ function printMatrix() {
 
 // Create the matrix table
 function createTable() {
-  $tableHead.empty();
   $tableBody.empty();
-  $tableHead.append('<td><strong>GrDegs</strong></td>');
-  for (let col=0; col < gradedDegreesTop.length; col++) {
-    $tableHead.append('<th order="' + gradedDegreesTop[col] + '" id="col' + gradedDegreesTop[col] + '">' + gradedDegreesTop[col] + '</th>');
-  }
-
-  for (let row=0; row < mat.length; row++) {
+  // By Columns
+  if (!shiftPressed) {
     let tableRow = '<tr>';
-    tableRow += '<th>' + gradedDegreesLeft[row] + '</th>';
+    tableRow += '<td id="border-maker" class="by-cols"><table class="inner-table"><tr id="inner-border-maker" class="inner-border-col"><th>GrDgs</th></tr>';
+    for (let i=0; i < gradedDegreesLeft.length; i++) {
+      tableRow += '<tr id="inner-border-maker" class="inner-border-col"><th>' + gradedDegreesLeft[i] + '</th></tr>';
+    }
+    tableRow += '</table></td>';
     for (let col=0; col < mat[0].length; col++) {
-      tableRow += '<td class="by-cols">' + mat[row][col] + '</td>';
+      tableRow += '<td id="border-maker" class="by-cols movers"><table class="inner-table">';
+      tableRow += '<tr id="inner-border-maker" class="inner-border-col"><th>' + gradedDegreesTop[col] + '</th></tr>';
+      for (let row=0; row < mat.length; row++) {
+        tableRow += '<tr id="inner-border-maker" class="inner-border-col"><td>' + mat[row][col] + '</td></tr>';
+      }
+      tableRow += '</table></td>';
     }
     tableRow += '</tr>';
     $tableBody.append(tableRow);
+    sortableByColumns();
+  // By Rows
+  } else {
+    let tableRow;
+    tableRow += '<tr id="border-maker" class="by-rows"><td><table class="inner-table"><tr><th id="inner-border-maker" class="inner-border-row">GrDgs</th>';
+    for (let i=0; i < gradedDegreesTop.length; i++) {
+      tableRow += '<th id="inner-border-maker" class="inner-border-row">' + gradedDegreesTop[i] + '</th>';
+    }
+    tableRow += '</tr></table></td></tr>'
+    for (let row=0; row < mat.length; row++) {
+      tableRow += '<tr id="border-maker" class="by-rows movers"><td><table class="inner-table"><tr>';
+      tableRow += '<th id="inner-border-maker" class="inner-border-row">' + gradedDegreesLeft[row] + '</th>';
+      for (let col=0; col < mat[0].length; col++) {
+        tableRow += '<td id="inner-border-maker" class="inner-border-row">' + mat[row][col] + '</td>';
+      }
+      tableRow += '</tr></table></td></tr>';
+    }
+    $tableBody.append(tableRow);
+    sortableByRows();
   }
-  sortableByRows();
-  $tableBody.sortable('disable');
-  sortableByColumns();
 }
 
 function switchToRows() {
-  $tableBody.find('td').each(function () {
+  $tableBody.find('#border-maker').each(function () {
     $(this).removeClass("by-cols");
     $(this).addClass("by-rows");
+  });
+  $innerTable.find('#inner-border-maker').each(function() {
+    $(this).addClass("inner-border-row");
+    $(this).removeClass("inner-border-col");
   });
 }
 
 function switchToCols() {
-  $tableBody.find('td').each(function () {
+  $tableBody.find('#border-maker').each(function () {
     $(this).removeClass("by-rows");
     $(this).addClass("by-cols");
   });
+  $innerTable.find('#inner-border-maker').each(function() {
+    $(this).removeClass("inner-border-row");
+    $(this).addClass("inner-border-col");
+  });
+
 }
 
 function sortableByColumns() {
   // make the table sortable by the headers and make the columns follow as the
   // headers move
-  $tableHead.sortable({
+  $tableBody.sortable({
     axis: "x" ,
-    items: 'th',
+    items: '.movers',
     cursor: 'move',
     helper: 'clone',
     distance: 1,
     opacity: 0.8,
-    placeholder: 'ui-state-highlight',
+    // placeholder: 'ui-state-highlight',
     // when selecting a column to sort this makes sure to grab its index
     // before moving it
-    start: function(event, ui) {
-        startPos = $tableHead.find('th').index(ui.item);
-        console.log($tableHead.find('th'));
-        oldPos = startPos;
-    },
-    // instead of on change we should do something on stop so that it can do
-    // it all in one go once you let go of the column header
-    change: function(event, ui) {
-        // Get position of the placeholder
-        let newPos = $tableHead.find('th').index($tableHead.find('th.ui-state-highlight'));
+    // start: function(event, ui) {
+    //     startPos = $tableHead.find('th').index(ui.item);
+    //     console.log($tableHead.find('th'));
+    //     oldPos = startPos;
+    // },
+    // // instead of on change we should do something on stop so that it can do
+    // // it all in one go once you let go of the column header
+    // change: function(event, ui) {
+    //     // Get position of the placeholder
+    //     let newPos = $tableHead.find('th').index($tableHead.find('th.ui-state-highlight'));
 
-        // If the position is right of the original position, substract it by one in cause of the hidden th
-        if (newPos>startPos) newPos--;
-        updateInteralMatrixByCol(oldPos, newPos);
+    //     // If the position is right of the original position, substract it by one in cause of the hidden th
+    //     if (newPos>startPos) newPos--;
+    //     updateInteralMatrixByCol(oldPos, newPos);
 
-        // move all the row elements
-        $tableBody.find('tr').find('td:eq(' + oldPos + ')').each(function() {
-            let tdElement = $(this);
-            let tdElementParent = tdElement.parent();
-            if(newPos>oldPos)// Move it the right
-                tdElementParent.find('td:eq(' + newPos + ')').after(tdElement);
-            else// Move it the left
-                tdElementParent.find('td:eq(' + newPos + ')').before(tdElement);
-        });
-        oldPos = newPos;
-    },
+    //     // move all the row elements
+    //     $tableBody.find('tr').find('td:eq(' + oldPos + ')').each(function() {
+    //         let tdElement = $(this);
+    //         let tdElementParent = tdElement.parent();
+    //         if(newPos>oldPos)// Move it the right
+    //             tdElementParent.find('td:eq(' + newPos + ')').after(tdElement);
+    //         else// Move it the left
+    //             tdElementParent.find('td:eq(' + newPos + ')').before(tdElement);
+    //     });
+    //     oldPos = newPos;
+    // },
   });
 }
 
 function sortableByRows() {
     $tableBody.sortable({
     axis: "y" ,
-    items: 'tr',
+    items: '.movers',
     cursor: 'move',
     helper: 'clone',
     distance: 1,
     opacity: 0.8,
-    placeholder: 'ui-state-highlight'
+    // placeholder: 'ui-state-highlight'
     // when selecting a column to sort this makes sure to grab its index
     // before moving it
     // start: function(event, ui) {
@@ -199,13 +226,13 @@ function updateInteralMatrixByRow(oldPos, newPos) {
   temp = gradedDegreesLeft[oldPos];
   gradedDegreesLeft[oldPos] = gradedDegreesLeft[newPos];
   gradedDegreesLeft[newPos] = temp;
-  
+
   for (let col=0; col < mat[0].length; col++){
     temp = mat[newPos][col];
     mat[newPos][col] = mat[oldPos][col];
     mat[oldPos][col] = temp;
   }
-  
+
   printMatrix();
 }
 
@@ -215,13 +242,13 @@ function updateInteralMatrixByCol(oldPos, newPos) {
   temp = gradedDegreesTop[oldPos];
   gradedDegreesTop[oldPos] = gradedDegreesTop[newPos];
   gradedDegreesTop[newPos] = temp;
-  
+
   for (let row=0; row < mat.length; row++){
     temp = mat[row][newPos];
     mat[row][newPos] = mat[row][oldPos];
     mat[row][oldPos] = temp;
   }
-  
+
   printMatrix();
 }
 
